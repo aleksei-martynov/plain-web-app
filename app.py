e8a0002f6d7cf3c3ef2f7a88eb4433e1332e021e
aleksei-martynov <aleksei.martynov@gmail.com>
2026-03-28 15:15:00 +0300

    Изменён маршрут на /hello

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = FastAPI()
 html_path = Path(__file__).parent / "templates" / "index.html"
 
 
-@app.get("/")
-async def root():
+@app.get("/hello")
+async def hello():
     html_content = html_path.read_text(encoding="utf-8")
     return HTMLResponse(content=html_content)
